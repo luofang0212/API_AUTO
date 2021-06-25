8a52cdedb8206486931bc0bb40b5f626eb0c15e1
luofang0212 <warm_homel@163.com>
2021-06-25 09:53:42 +0800

    增加发送邮件
    Signed-off-by: luofang0212 <warm_homel@163.com>

--- util/get_path.py
+++ util/get_path.py
@@ -9,7 +9,7 @@ import pathlib
 # 获取路径
 '''
 1: os.path.split(path)——返回路径的目录和文件名，即将目录和文件名分开，而不是一个整体。此处只是把前后两部分分开而已。就是找最后一个'/'。
-2: os.path.join(project_path,'test_result','test_report')  拼接路径
+2: os.path.join(project_path,'test_result','html_report')  拼接路径
 '''
 # 获取文件当前路径
 file_path = os.path.realpath(__file__)
@@ -20,13 +20,13 @@ project_path = os.path.split(os.path.split(file_path)[0])[0]
 test_excel_path = os.path.join(project_path, 'test_data', 'testData.xlsx')
 
 # 测试报告路径
-test_report_path = os.path.join(project_path, 'test_result', 'test_report', 'test_report_1.html')
+test_report_path = os.path.join(project_path, 'test_result', 'html_report', 'test_report_1.html')
 
 # 配置文件路径
 read_config_path = os.path.join(project_path, 'config', 'case.config')
 
-# 日志文件路径
+# 日志文件名生成
 timestamp = time.strftime("%Y-%m-%d", time.localtime())
-logfilename = '%s.txt' % timestamp
-
+logfilename = 'api_auto' + '%s.log' % timestamp
+# 日志文件路径
 log_file_path = os.path.join(project_path, 'test_result', 'logs', logfilename)
---
+++ util/send_email.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import time
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
+from util import get_path
+from util.do_logs import TestLog
+
+log = TestLog().getlog()
+
+
+# 发送带附件邮件
+class SendEmail:
+
+    def send_email(self):
+
+        now = time.strftime('%Y-%m-%d_%H:%M:%S')  # 获取时间戳
+        # 设置服务器所需信息
+        mail_host = 'smtp.163.com'  # 163邮箱服务器地址
+        mail_user = 'warm_homel@163.com'  # 163用户名
+        mail_pass = 'love920212'  # 密码(部分邮箱为授权码)
+        sender = 'warm_homel@163.com'  # 邮件发送方邮箱地址
+        receivers = ['272457044@qq.com']  # 邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
+
+        # 设置email信息
+        # 添加一个MIMEmultipart类，处理正文及附件
+        message = MIMEMultipart()
+        message['Subject'] = now + '测试报告'  # 邮件主题
+        message['From'] = sender  # 发送方信息
+        message['To'] = receivers[0]  # 接受方信息
+
+        # 文本正文
+        content = now + '测试报告，请查收！！！'
+        part1 = MIMEText(content, 'plain', 'utf-8')  # 邮件内容设置
+        message.attach(part1)
+
+        # 添加附件附件
+        filepath = get_path.test_report_path
+        part2 = MIMEApplication(open(filepath,'rb').read())
+        part2.add_header('Content-Disposition','attachment',filename='test_report.html')
+        message.attach(part2)
+
+        # 登录并发送邮件
+        try:
+            smtpObj = smtplib.SMTP()
+            # 连接到服务器
+            smtpObj.connect(mail_host, 25)
+            # 登录到服务器
+            smtpObj.login(mail_user, mail_pass)
+            # 发送
+            smtpObj.sendmail(
+                sender, receivers, message.as_string())
+            # 退出
+            smtpObj.quit()
+            log.info('邮件成功发送到：{0}'.format(receivers))
+
+        except smtplib.SMTPException as e:
+            log.info('邮件发送失败：{0}'.format(e))
+            raise e
+
+
+if __name__ == '__main__':
+    SendEmail().send_email()
